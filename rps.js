a0ee40e1720261a1ae07e310c0e49c2d001afbd3
Daniel Lee <57880066+jughkoop@users.noreply.github.com>
2025-07-25 14:26:11 -0600

    Add full game logic for 5-round RPS

--- rps.js
+++ rps.js
@@ -1,41 +1,59 @@
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log("Tie! Try again.")
-        playRound(getHumanChoice, computerChoice);
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log("Tie! Try again.")
+            playRound(getHumanChoice, computerChoice);
+        }
+        else if (humanChoice === "rock") {
+            if (computerChoice === "paper") {
+                console.log("You lose! Paper beats Rock.")
+                computerScore++;
+            }
+            else {
+                console.log("You win! Rock beats Scissors.")
+                humanScore++;
+            }
+        }
+        else if (humanChoice === "paper") {
+            if (computerChoice === "rock") {
+                console.log("You win! Paper beats Rock.")
+                humanScore++;
+            }
+            else {
+                console.log("You lose! Scissors beats Paper.")
+                computerScore++;
+            }
+        }
+        else if (humanChoice === "scissors") {
+            if (computerChoice === "rock") {
+                console.log("You lose! Rock beats Scissors.")
+                computerScore++;
+            }
+            else {
+                console.log("You win! Scissors beats Paper.")
+                humanScore++;
+            }
+        }
     }
-    else if (humanChoice === "rock") {
-        if (computerChoice === "paper") {
-            console.log("You lose! Paper beats Rock.")
-            computerScore++;
-        }
-        else {
-            console.log("You win! Rock beats Scissors.")
-            humanScore++;
-        }
+
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        playRound(humanChoice, computerChoice);
     }
-    else if (humanChoice === "paper") {
-        if (computerChoice === "rock") {
-            console.log("You win! Paper beats Rock.")
-            humanScore++;
-        }
-        else {
-            console.log("You lose! Scissors beats Paper.")
-            computerScore++;
-        }
+
+    if(humanScore > computerScore) {
+        console.log("YOU WIN!")
     }
-    else if (humanChoice === "scissors") {
-        if (computerChoice === "rock") {
-            console.log("You lose! Rock beats Scissors.")
-            computerScore++;
-        }
-        else {
-            console.log("You win! Scissors beats Paper.")
-            humanScore++;
-        }
+    else {
+        console.log("YOU LOSE")
     }
+    console.log(humanScore);
+    console.log(computerScore);
 }
 
 function getComputerChoice() {
